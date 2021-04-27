06eeb77d1a2a1fd34c69f239f5697dc66b8d56c3
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-04-26 20:42:50 -0400

    Create and style first section of Contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import './Contact.scss'
 
 export default function Contact() {
   return (
-    <div>
-      
-    </div>
+    <main className="contact">
+      <div className="contact__first">
+        <p className="contact__heading">Still not sure if this is right for you?</p>
+        <p className="contact__heading">Can’t decide which services are best for you?</p>
+        <p className="contact__text">Feel free to contact me by</p>
+        <a className="contact__button">Email</a>
+        <p className="contact__text">Or connect with me through</p>
+        <a className="contact__button">Instagram</a>
+        <p className="contact__text">Or fill out the form below!</p>
+      </div>
+    </main>
   )
 }

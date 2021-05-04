f883656b253044173943df458419bbe47f1b6be8
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-05-04 13:24:43 -0400

    Create route for getting all appointments

--- client/src/pages/Book/Book.js
+++ client/src/pages/Book/Book.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Book.scss'
 import { useAuth } from '../../contexts/AuthContext'
-
+import axios from 'axios'
 
 export default function Book() {
 
   const { currentUser } = useAuth()
+  
+  useEffect(()=>{
+    window.scrollTo(0,0)
+  }, [])
 
   return (
     <main className="book">
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 const app = express();
 const PORT = process.env.PORT || 8070;
@@ -9,6 +11,11 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send("Working")
+})
+
+app.get('/appointments', async (req, res) => {
+  const result = await prisma.appointments.findMany()
+  res.json(result)
 })
 
 app.listen(PORT, () => {

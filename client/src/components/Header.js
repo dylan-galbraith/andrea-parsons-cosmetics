87b4767a83b072de0846e3195b89ea167dceccee
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-05-05 17:54:44 -0400

    Style Header for mobile devices

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import logo from '../assets/images/logo.png'
 
 export default function Header() {
+
+  const [ open, setOpen ] = useState(false)
+
+  function toggleMenu() {
+    setOpen(!open)
+  }
+
   return (
     <div className="header">
-      <div className="header__logo">
-        <h1 className="header__heading">Andrea Parsons</h1>
-        <h2 className="header__subheading">cosmetic nurse injector</h2>
-      </div>
-      <nav className="header__nav">
-        <NavLink className="header__nav__link" to='/' exact>HOME</NavLink>
-        <NavLink className="header__nav__link" to='/book'>BOOK</NavLink>
-        <NavLink className="header__nav__link" to='/services'>SERVICES</NavLink>
-        <NavLink className="header__nav__link" to='/about'>ABOUT</NavLink>
-        <NavLink className="header__nav__link" to='/contact'>CONTACT</NavLink>
-        <NavLink className="header__nav__link" to='/faq'>FAQ</NavLink>
+      <img className="header__logo" src={logo} alt="logo" />
+      <button onClick={toggleMenu} className="header__button">{open ? "CLOSE" : "MENU"}</button>
+      <nav className={open ? "header__nav" : "hidden"}>
+        <NavLink onClick={toggleMenu} className="header__nav__link" to='/' exact>HOME</NavLink>
+        <NavLink onClick={toggleMenu} className="header__nav__link" to='/book'>BOOK</NavLink>
+        <NavLink onClick={toggleMenu} className="header__nav__link" to='/services'>SERVICES</NavLink>
+        <NavLink onClick={toggleMenu} className="header__nav__link" to='/about'>ABOUT</NavLink>
+        <NavLink onClick={toggleMenu} className="header__nav__link" to='/contact'>CONTACT</NavLink>
+        <NavLink onClick={toggleMenu} className="header__nav__link" to='/faq'>FAQ</NavLink>
       </nav>
     </div>
   )

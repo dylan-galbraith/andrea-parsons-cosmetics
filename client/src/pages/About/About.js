10b3f6a974f101de3be4101df8cd006f61eb9888
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-05-05 18:48:31 -0400

    Style About page for all devices

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -13,6 +13,7 @@ export default function About() {
     <main className="about">
       <div className="about__first">
         <div className="about__bio">
+          <img className="about__image--mobile" src={headshot} alt=""/>
           <p className="about__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque id nibh tortor id. Senectus et netus et malesuada fames ac turpis. Velit scelerisque in dictum non. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus. Sit amet mauris commodo quis imperdiet massa.</p>
           <a href="https://www.instagram.com/cosmetic.nurse.ange/" className="about__button" target="_blank" rel="noopener noreferrer">Follow on Instagram</a>
           <p className="about__text">Lacus viverra vitae congue eu consequat ac felis donec et. Pellentesque elit eget gravida cum sociis natoque penatibus et. Ipsum faucibus vitae aliquet nec ullamcorper.</p>
@@ -20,7 +21,7 @@ export default function About() {
         <img className="about__image" src={headshot} alt=""/>
       </div>
       <div className="about__second">
-        <p className="about__services">Take a look at our services!</p>
+        <p className="about__services">Take a look at my services!</p>
         <Link to='/services' className="about__button about__button--brown">SERVICES</Link>
       </div>
       <div className="about__third">

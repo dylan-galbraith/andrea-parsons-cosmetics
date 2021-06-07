cbc0701268da7ebefaee5a1678fca865a31f6171
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-06-07 18:26:36 -0400

    Update About page content

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import './About.scss'
-import headshot from '../../assets/images/ap-dog-2.jpg'
+import headshot from '../../assets/images/image4.jpeg'
 import { Link } from 'react-router-dom'
 
 export default function About() {
@@ -14,9 +14,9 @@ export default function About() {
       <div className="about__first">
         <div className="about__bio">
           <img className="about__image--mobile" src={headshot} alt=""/>
-          <p className="about__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque id nibh tortor id. Senectus et netus et malesuada fames ac turpis. Velit scelerisque in dictum non. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus. Sit amet mauris commodo quis imperdiet massa.</p>
+          <p className="about__text">Hi there! Thanks for stopping by my website. I am Andrea, a registered nurse since 2018. I have always dreamed of working in the aesthetic side of medicine; and after gaining experience in acute care, I figured that now was the best time! I currently work in the emergency department, where I get to engage with tons of different personalities and utilize various skill sets. I love learning and expanding my practice- and can’t wait to continue to do so with aesthetics!  </p>
           <a href="https://www.instagram.com/cosmetic.nurse.ange/" className="about__button" target="_blank" rel="noopener noreferrer">Follow on Instagram</a>
-          <p className="about__text">Lacus viverra vitae congue eu consequat ac felis donec et. Pellentesque elit eget gravida cum sociis natoque penatibus et. Ipsum faucibus vitae aliquet nec ullamcorper.</p>
+          <p className="about__text">I can’t wait to hopefully meet you and make you feel your most beautiful! Please do not hesitate to reach out with any questions.</p>
         </div>
         <img className="about__image" src={headshot} alt=""/>
       </div>
@@ -24,7 +24,7 @@ export default function About() {
         <p className="about__services">Take a look at my services!</p>
         <Link to='/services' className="about__button about__button--brown">SERVICES</Link>
       </div>
-      <div className="about__third">
+      {/* <div className="about__third">
         <div className="about__review">
           <p className="about__text">“Lacus viverra vitae congue eu consequat ac felis donec et. Pellentesque elit eget gravida cum sociis natoque penatibus et.”</p>
           <p className="about__name">- Mariah Lepore</p>
@@ -33,7 +33,7 @@ export default function About() {
           <p className="about__text">“Pellentesque id nibh tortor id. Senectus et netus et malesuada fames ac turpis. Velit scelerisque in dictum non.”</p>
           <p className="about__name">- Dylan Galbraith</p>
         </div>
-      </div>
+      </div> */}
     </main>
   )
 }

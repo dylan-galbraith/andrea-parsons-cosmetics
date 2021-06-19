ec59888eecc2f667296594abb484173a30e949df
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-06-19 13:44:46 -0400

    Style about page for all devices

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -13,8 +13,8 @@ export default function About() {
     <main className="about">
       <div className="about__first">
         <div className="about__bio">
-          <img className="about__image--mobile" src={headshot} alt=""/>
           <h1 className="about__heading">About Myself</h1>
+          <img className="about__image--mobile" src={headshot} alt=""/>
           <p className="about__text">Hi there! Thanks for stopping by my website. I am Andrea, a registered nurse since 2018. I have always dreamed of working in the aesthetic side of medicine; and after gaining experience in acute care, I figured that now was the best time! I currently work in the emergency department, where I get to engage with tons of different personalities and utilize various skill sets. I love learning and expanding my practice- and can’t wait to continue to do so with aesthetics!  </p>
           <a href="https://www.instagram.com/cosmetic.nurse.ange/" className="about__button" target="_blank" rel="noopener noreferrer">Follow on Instagram</a>
           <p className="about__text">I can’t wait to hopefully meet you and make you feel your most beautiful! Please do not hesitate to reach out with any questions.</p>
